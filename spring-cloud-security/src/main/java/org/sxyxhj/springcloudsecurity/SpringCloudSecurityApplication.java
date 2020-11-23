e9403dc2cc981b53afee48d9f3786a288a64780b
sxyxhj <sxyxhj@outlook.com>
2020-11-23 22:34:23 +0800

    add Spring Security Demo

---
+++ spring-cloud-security/src/main/java/org/sxyxhj/springcloudsecurity/SpringCloudSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.sxyxhj.springcloudsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudSecurityApplication.class, args);
+    }
+
+}
